b45b9d5859188d82ddb64730a9895b17d13efe67
Andreas Müller <schnitzeltony@googlemail.com>
2012-06-25 13:13:42 +0200

    card-part.sh: add ext4 for rootfs type and make it default

--- script-and-docs/card-part.sh
+++ script-and-docs/card-part.sh
@@ -10,14 +10,46 @@
 # for debugging set DEBUG=echo
 DEBUG=
 
+# SelectRootFileSystem() creates a dialog to select one of all available removable
+# devices. The path to the selected device is stored in the variale DevicePath.
+SelectRootFileSystem() {
+	strSelection="0 ext4 1 ext3"
+	dialog --title 'Select rootfs type'\
+	--menu 'Move using [UP] [DOWN],[Enter] to select' 10 100 2\
+	${strSelection}\
+	2>/tmp/menuitem.$$
+
+	# get OK/Cancel
+	sel=$?
+	# get selected menuitem
+	menuitem=`cat /tmp/menuitem.$$`
+	rm -f /tmp/menuitem.$$
+
+	# Cancel Button or <ESC>
+	if [ $sel -eq 1 -o $sel -eq 255 ] ; then
+		echo 'Cancel selected SelectRootFileSystem()'
+		return 1
+	fi
+	# Selection
+	case $menuitem in
+		0) RootfsType='ext4';;
+		1) RootfsType='ext3';;
+		*) echo "Unknown type"; exit 1;;
+	esac
+}
+
 run_user() {
 	if [ -z $DevicePath ]; then
 		SelectCardDevice || exit 1
-	elif [ $# -gt 1 ]; then
-		echo "Usage: $0 [Card device path]"
+	fi
+	if [ -z $RootfsType ]; then
+		SelectRootFileSystem || exit 1
+	fi
+	if [ $# -gt 2 ]; then
+		echo "Usage: $0 [Card device path] [Rootfs type]"
 		exit 1;
 	fi
-	RootParams="$DevicePath"
+	RootParams="$DevicePath $RootfsType"
 }
 
 run_root() {
@@ -26,6 +58,13 @@ run_root() {
 		echo "$DevicePath is not a valid block device!"
 		exit 1
 	fi
+
+	# file system valid?
+	case $RootfsType in
+		ext3) ;;
+		ext4) ;;
+		*) echo "Unknown rootfs type"; exit 1;;
+	esac
 
 	# check if the card is currently mounted
 	MOUNTSTR=$(mount | grep $DevicePath)
@@ -49,31 +88,13 @@ run_root() {
 	} | $DEBUG sfdisk -D -H 255 -S 63 -C $CYLINDERS $DevicePath
 	# write partitions
 	$DEBUG mkfs.vfat -F 32 -n "boot" ${DevicePath}1
-	$DEBUG mke2fs -j -t ext3 -L "rootfs" ${DevicePath}2
-
+	$DEBUG mke2fs -j -t $RootfsType -L "rootfs" ${DevicePath}2
 }
 
 
 source `dirname $0`/tools.inc
 DevicePath=$1
+RootfsType=$2
 # On the 1st call: run user
 # After the 2nd call: run root
 chk_root && run_root
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
